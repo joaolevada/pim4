135f5033f8347ae1129938f1b2ba0cf61d52eed8
Guilherme11gr <guilherme11.gr@gmail.com>
2018-10-18 18:28:39 -0300

    Alterações na paleta de cores e adição da tela de login

--- frontend/web/.eslintrc.js
+++ frontend/web/.eslintrc.js
@@ -9,12 +9,8 @@ module.exports = {
     "rules": {
         "no-unused-vars": "off",
         "prefer-destructuring": "off",
-<<<<<<< HEAD
-        "linebreak-style": "off"
-=======
+        "linebreak-style": "off",
         "padded-blocks":"off"
->>>>>>> bbcc32c0e52cbd1e4a8bf271c9d0782ce2393221
-      
     },
     "globals": {
         "doc": true,
