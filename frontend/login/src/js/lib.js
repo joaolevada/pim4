724076f5aee040925edd51a960f42b0d4dad30f9
Guilherme11gr <guilherme11.gr@gmail.com>
2018-10-20 21:31:08 -0300

    Versão final da tela de login.

--- frontend/login/src/js/lib.js
+++ frontend/login/src/js/lib.js
@@ -1,11 +1,58 @@
-function validaLogin(form){
+function validaCampos(form) {
     form.password.addEventListener('keyup', e => {
 
-        if(form.email.value && String(form.password.value).length >= 8) {
-            btnLogar.classList.remove('disabled')
+        if (form.email.value && String(form.password.value).length >= 8) {
+            btnLogar.removeAttribute('disabled');
         }
         else {
-            btnLogar.classList.add('disabled');
+            addAttribute(btnLogar,'disabled');
         }
     });
+}
+
+function addAttribute(element, attr, value) {
+    let att = document.createAttribute(attr);
+    value ? att.value = value : null;
+    element.setAttributeNode(att);
+}
+
+async function getUser (URL , form) {
+
+    addAttribute(btnLogar,'disabled');
+
+    const progress = document.querySelector('.progress');
+    const emailHelp = document.querySelector('#emailHelp');
+
+    progress.classList.remove('d-none');
+
+    try {
+        const userToken = btoa(`${form.email.value}:${form.password.value}`);
+
+        const HEADER = {
+            method: "GET", 
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': userToken
+            }
+        };
+
+        const response = await fetch(URL, HEADER);
+
+        if(response.ok) {
+            const user = await response.json();
+            localStorage.setItem('token',user.token);
+            progress.classList.add('d-none');
+            window.location = "https://pimquatro.github.io/App/";
+            
+        } else {
+            progress.classList.add('d-none');
+            form.email.classList.add('is-invalid');
+            form.password.classList.add('is-invalid');
+            emailHelp.classList.remove('d-none');
+            btnLogar.removeAttribute('disabled');
+        }
+
+    } catch (err) {
+        console.error(err); // Dev only
+    }
 }
