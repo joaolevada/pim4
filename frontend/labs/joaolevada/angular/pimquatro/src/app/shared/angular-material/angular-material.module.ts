2c66790c7f0a0651d7f51bb5026aad59d3be1a15
Joao Levada <joaolevada@gmail.com>
2018-10-23 13:29:21 -0300

    Criei módulo inicial com a declaração dos componentes do Angular Material.

---
+++ frontend/labs/joaolevada/angular/pimquatro/src/app/shared/angular-material/angular-material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule
+  ],
+  exports: [
+    MatButtonModule
+  ],
+  declarations: []
+})
+export class AngularmaterialModule { }
