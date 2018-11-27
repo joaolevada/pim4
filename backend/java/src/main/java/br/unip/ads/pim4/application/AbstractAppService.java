2aa03308d0c3e03fc4457bf39795891820681d65
Joao Levada <joaolevada@gmail.com>
2018-11-27 17:01:59 -0200

    Movi o codigo do backend para outro repositório.

--- backend/java/src/main/java/br/unip/ads/pim4/application/AbstractAppService.java
+++
@@ -1,5 +0,0 @@
-package br.unip.ads.pim4.application;
-
-public abstract class AbstractAppService {
-
-}
--- backend/java/src/main/java/br/unip/ads/pim4/application/chamado/dto/ChamadoDetalhadoDto.java
+++
@@ -1,5 +0,0 @@
-package br.unip.ads.pim4.application.chamado.dto;
-
-public class ChamadoDetalhadoDto {
-
-}
